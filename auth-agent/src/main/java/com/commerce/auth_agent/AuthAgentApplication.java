32d38cdfcca669c1ef4c87defe8e55aa1644859d
Shinnybest <kim295521@naver.com>
2024-07-01 15:48:54 +0900

    initial commit

---
+++ auth-agent/src/main/java/com/commerce/auth_agent/AuthAgentApplication.java
@@ -0,0 +1,13 @@
+package com.commerce.auth_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthAgentApplication.class, args);
+	}
+
+}
