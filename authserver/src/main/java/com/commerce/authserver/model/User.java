9f20143f3d7cc22d2fc6a55f39b758da2bea71e2
Shinnybest <kim295521@naver.com>
2024-07-04 16:58:37 +0900

    [ISSUE-#004] 사용자 등록을 위한 User Entity 추가 및 BaseEntity 정의 -- @EnableJpaAuditing 어노테이션 애플리케이션 클래스에 추가

---
+++ authserver/src/main/java/com/commerce/authserver/model/User.java
@@ -0,0 +1,42 @@
+package com.commerce.authserver.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Table(name = "user")
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "is_deleted")
+    private boolean isDeleted = false;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Builder
+    public User(String email, String username, String password) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+}
