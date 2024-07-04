2fa14ca78ca39f758d3324ffa65fc8561032268f
Shinnybest <kim295521@naver.com>
2024-07-04 17:20:43 +0900

    [ISSUE-#10] UserDetails, UserDetailsService 구현

---
+++ authserver/src/main/java/com/commerce/authserver/authentication/CustomUserDetails.java
@@ -0,0 +1,33 @@
+package com.commerce.authserver.authentication;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+@Component
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private String username;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+}
