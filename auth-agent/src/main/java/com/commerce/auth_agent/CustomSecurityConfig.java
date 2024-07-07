650404963e12ecfbe2075080a5b7bf20f8479f90
Shinnybest <kim295521@naver.com>
2024-07-07 18:39:32 +0900

    [NO-ISSUE] Security Config에 필요한 의존성 Bean 주입

--- auth-agent/src/main/java/com/commerce/auth_agent/CustomSecurityConfig.java
+++ auth-agent/src/main/java/com/commerce/auth_agent/CustomSecurityConfig.java
@@ -2,21 +2,26 @@ package com.commerce.auth_agent;
 
 import com.commerce.auth_agent.authentication.after.AuthenticationSuccessHandler;
 import com.commerce.auth_agent.authorization.after.HttpCookieOAuth2AuthorizationRequestRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class CustomSecurityConfig {
+
+    private final AuthenticationSuccessHandler authenticationSuccessHandler;
+    private final HttpCookieOAuth2AuthorizationRequestRepository authorizationRequestRepository;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .oauth2Login(oauth2 -> oauth2
                 .authorizationEndpoint(endpoint -> endpoint
-                        .authorizationRequestRepository(new HttpCookieOAuth2AuthorizationRequestRepository()))
-                .successHandler(new AuthenticationSuccessHandler()));
+                        .authorizationRequestRepository(authorizationRequestRepository))
+                .successHandler(authenticationSuccessHandler));
 
         return http.build();
     }
